0e3b7dda60ee662928a7fc87a69b92a6c2a45f6f
Johnny Allen <johnny.allen@daveramsey.com>
2022-11-18 21:25:00 -0600

    Remove unused code

--- 2021/ruby/lib/advent_of_code.rb
+++ 2021/ruby/lib/advent_of_code.rb
@@ -3,8 +3,6 @@ require "active_support"
 
 require_relative "advent_of_code/day_one"
 require_relative "advent_of_code/day_two"
-require_relative "advent_of_code/day_two/submarine_positioning_part_one"
-require_relative "advent_of_code/day_two/submarine_positioning_part_two"
 require_relative "advent_of_code/day_three"
 
 module AdventOfCode
