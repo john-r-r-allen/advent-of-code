30b2d129186d4d02830e28f08354bba8f05445dc
Johnny Allen <johnny.allen@daveramsey.com>
2022-12-03 07:19:11 -0600

    Get item priority working

--- 2022/ruby/lib/advent_of_code/day_three.rb
+++ 2022/ruby/lib/advent_of_code/day_three.rb
@@ -1,9 +1,16 @@
 module AdventOfCode
   class DayThree
-    attr_reader :puzzle_input
+    attr_reader :items_in_rucksack
 
     def initialize(puzzle_input_path)
-      @puzzle_input = CSV.read(puzzle_input_path).map(&:first)
+      @items_in_rucksack = CSV.read(puzzle_input_path).map(&:first)
+    end
+
+    def item_priority(original_item)
+      "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".split("").each_with_index do |item, index|
+        priority = index + 1
+        return priority if item == original_item
+      end
     end
 
     def part_one
--- 2022/ruby/spec/lib/advent_of_code/day_three_spec.rb
+++ 2022/ruby/spec/lib/advent_of_code/day_three_spec.rb
@@ -2,7 +2,23 @@ RSpec.describe AdventOfCode::DayThree do
   let(:indent) { "\t" }
   let(:input_file_name) { "day_three.csv" }
 
-  fcontext "#part_one" do
+  fcontext "#item_priority" do
+    let(:subject) { described_class.new("../inputs/sample_inputs/#{input_file_name}") }
+
+    it "returns 1-26 for a-z (1 for a, 2 for b,... 26 for z)" do
+      expect(subject.item_priority("a")).to eq(1)
+      expect(subject.item_priority("b")).to eq(2)
+      expect(subject.item_priority("z")).to eq(26)
+    end
+
+    it "returns 27-52 for A-Z (27 for A, 28 for B,... 52 for Z)" do
+      expect(subject.item_priority("A")).to eq(27)
+      expect(subject.item_priority("B")).to eq(28)
+      expect(subject.item_priority("Z")).to eq(52)
+    end
+  end
+
+  context "#part_one" do
     context "with the real puzzle input" do
       let(:subject) { described_class.new("../inputs/#{input_file_name}") }
 
