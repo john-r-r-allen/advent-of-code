5d0161976d0142b21f633050cd2815b8342ecdae
Johnny Allen <johnny.allen@daveramsey.com>
2022-12-03 12:53:57 -0600

    Add pending tests for remaining days in 2022

--- 2022/ruby/spec/lib/advent_of_code/day_twenty_spec.rb
+++ 2022/ruby/spec/lib/advent_of_code/day_twenty_spec.rb
@@ -1,2 +1,51 @@
 RSpec.describe AdventOfCode::DayTwenty do
+  let(:indent) { "\t" }
+  let(:input_file_name) { "day_twenty.csv" }
+  let(:sample_puzzle_input) { "../inputs/sample_inputs/#{input_file_name}" }
+  let(:real_puzzle_input) { "../inputs/#{input_file_name}" }
+  let(:subject) { described_class.new(sample_puzzle_input) }
+
+  xdescribe "#part_one" do
+    context "with the real puzzle input" do
+      let(:subject) { described_class.new(real_puzzle_input) }
+
+      it "returns the expected result" do
+        output = subject.part_one
+
+        puts "#{indent}part one output: #{output}"
+        expect(output).to eq(0)
+      end
+    end
+
+    context "with the sample puzzle input" do
+      it "returns the expected result" do
+        output = subject.part_one
+
+        puts "#{indent}part one output: #{output}"
+        expect(output).to eq(0)
+      end
+    end
+  end
+
+  xdescribe "#part_two" do
+    context "with the real puzzle input" do
+      let(:subject) { described_class.new(real_puzzle_input) }
+
+      it "returns the expected result" do
+        output = subject.part_two
+
+        puts "#{indent}part two output: #{output}"
+        expect(output).to eq(0)
+      end
+    end
+
+    context "with the sample puzzle input" do
+      it "returns the expected result" do
+        output = subject.part_two
+
+        puts "#{indent}part two output: #{output}"
+        expect(output).to eq(0)
+      end
+    end
+  end
 end
