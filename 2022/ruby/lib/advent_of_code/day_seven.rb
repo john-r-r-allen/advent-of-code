47df46c0a9cb3f422dfae27828248ab6f72ed395
Johnny Allen <johnny.allen@daveramsey.com>
2022-12-07 10:15:48 -0600

    Rubocop

--- 2022/ruby/lib/advent_of_code/day_seven.rb
+++ 2022/ruby/lib/advent_of_code/day_seven.rb
@@ -18,7 +18,7 @@ module AdventOfCode
     def initialize(puzzle_input_path)
       @terminal_contents = CSV.read(puzzle_input_path).map(&:first)
       @most_recent_command = nil
-      @root_node = Tree::TreeNode.new(ROOT_DIRECTORY, { type: DIR_TYPE } )
+      @root_node = Tree::TreeNode.new(ROOT_DIRECTORY, { type: DIR_TYPE })
       @current_node = nil
       @directories_with_sizes = {}
     end
@@ -26,7 +26,7 @@ module AdventOfCode
     def part_one
       process_terminal_contents
       determine_directory_sizes
-      directories_with_sizes.select { |_k,v| v <= 100_000 }.values.sum
+      directories_with_sizes.select { |_k, v| v <= 100_000 }.values.sum
     end
 
     def process_terminal_contents
@@ -53,7 +53,7 @@ module AdventOfCode
       return @current_node = root_node if first_directory_change?(command)
 
       directory_name = command[5..]
-      if command== CHANGE_DIRECTORY_COMMAND_START + PARENT_DIRECTORY
+      if command == CHANGE_DIRECTORY_COMMAND_START + PARENT_DIRECTORY
         raise "Attempting to navigate to the parent of the root directory" if current_node.root?
 
         @current_node = current_node.parent
@@ -84,26 +84,25 @@ module AdventOfCode
       find_size_and_name_space = terminal_content.index(" ")
       size = terminal_content[0..(find_size_and_name_space - 1)].to_i
       file_name = terminal_content[(find_size_and_name_space + 1)..]
-      @current_node << Tree::TreeNode.new(file_name, { type: FILE_TYPE, size: size })
+      @current_node << Tree::TreeNode.new(file_name, { type: FILE_TYPE, size: })
     end
 
     def determine_directory_sizes
       @directories_with_sizes[root_node.name] = determine_size_of_children(node: root_node, full_name: root_node.name)
     end
 
-    def determine_size_of_children(node:, full_name:)
+    def determine_size_of_children(node:, full_name:) # rubocop:disable Metrics/MethodLength
       size = 0
 
       node.children do |child|
-        if child.content[:type] == DIR_TYPE
+        case child.content[:type]
+        when DIR_TYPE
           size_of_child = determine_size_of_children(node: child, full_name: "#{full_name}#{child.name}/")
-          if @directories_with_sizes.key?("#{full_name}#{child.name}/")
-            binding.pry
-          else
-            @directories_with_sizes["#{full_name}#{child.name}/"] = size_of_child
-          end
+          raise "Unexpected code path followed." if @directories_with_sizes.key?("#{full_name}#{child.name}/")
+
+          @directories_with_sizes["#{full_name}#{child.name}/"] = size_of_child
           size += size_of_child
-        elsif child.content[:type] == FILE_TYPE
+        when FILE_TYPE
           size += child.content[:size]
         end
       end
@@ -116,7 +115,7 @@ module AdventOfCode
       determine_directory_sizes
       unused_space = TOTAL_DISK_SPACE - directories_with_sizes[ROOT_DIRECTORY]
       needed_additional_space = SPACE_NEEDED_FOR_UPDATE - unused_space
-      directories_with_sizes.select { |dir, space| space >= needed_additional_space  }.values.min
+      directories_with_sizes.select { |_dir, space| space >= needed_additional_space }.values.min
     end
   end
 end
