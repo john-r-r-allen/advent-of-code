85870ecebed0a6eec9c9055483d2a118c34efd0c
Johnny Allen <johnny.allen@daveramsey.com>
2022-12-10 16:06:04 -0600

    2022 Day Ten Parts One and Two working

--- 2022/ruby/lib/advent_of_code/day_ten.rb
+++ 2022/ruby/lib/advent_of_code/day_ten.rb
@@ -1,17 +1,90 @@
 module AdventOfCode
   class DayTen
-    attr_reader :puzzle_input
+    NO_OP = "noop".freeze
+    ADD_X = "addx".freeze
 
-    def initialize(puzzle_input_path)
-      @puzzle_input = CSV.read(puzzle_input_path).map(&:first)
+    attr_reader :cpu_instructions,
+                :x_register,
+                :signal_strengths,
+                :cycles_waited,
+                :cpu_instructions_position,
+                :performing_add_x
+
+    def initialize(cpu_instructions_path)
+      @cpu_instructions = CSV.read(cpu_instructions_path).map(&:first)
+      @x_register = 1
+      @signal_strengths = []
+      @cycles_waited = 0
+      @cpu_instructions_position = 0
+      @performing_add_x = false
     end
 
     def part_one
-      nil
+      determine_signal_strengths
+      signal_strengths.sum
+    end
+
+    def determine_signal_strengths
+      221.times do |cycle|
+        next if cycle.zero?
+
+        record_signal_strength(cycle) if check_signal_strength?(cycle)
+        perform_cpu_instruction(cpu_instructions[cpu_instructions_position])
+      end
+    end
+
+    def check_signal_strength?(cycle)
+      return false if cycle > 220
+
+      ((cycle - 20) % 40).zero?
+    end
+
+    def record_signal_strength(cycle)
+      current_signal_strength = cycle * x_register
+      signal_strengths << current_signal_strength
+    end
+
+    def perform_cpu_instruction(cpu_instruction)
+      raise "Invalid command" unless cpu_instruction.starts_with?(ADD_X) || cpu_instruction == NO_OP
+
+      return @cycles_waited += 1 if cpu_instruction.starts_with?(ADD_X) && cycles_waited.zero?
+
+      if cpu_instruction.starts_with?(ADD_X)
+        @cycles_waited = 0
+        v = cpu_instruction.split.last.to_i
+        @x_register += v
+      end
+
+      @cpu_instructions_position += 1
     end
 
     def part_two
-      nil
+      @x_register = 0
+      crt = ["", "", "", "", "", ""]
+      241.times do |cycle|
+        next if cycle.zero?
+
+        draw_pixel(cycle, crt)
+        perform_cpu_instruction(cpu_instructions[cpu_instructions_position])
+      end
+
+      crt
+    end
+
+    def draw_pixel(cycle, crt)
+      position = (cycle - 1) % 40
+      crt_row = 0 if 1 <= cycle && cycle <= 40
+      crt_row = 1 if 41 <= cycle && cycle <= 80
+      crt_row = 2 if 81 <= cycle && cycle <= 120
+      crt_row = 3 if 121 <= cycle && cycle <= 160
+      crt_row = 4 if 161 <= cycle && cycle <= 200
+      crt_row = 5 if 201 <= cycle && cycle <= 240
+
+      if [x_register, x_register + 1, x_register + 2].include?(position)
+        crt[crt_row] += "#"
+      else
+        crt[crt_row] += "."
+      end
     end
   end
 end
--- 2022/ruby/spec/lib/advent_of_code/day_ten_spec.rb
+++ 2022/ruby/spec/lib/advent_of_code/day_ten_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe AdventOfCode::DayTen do
   let(:real_puzzle_input) { "../inputs/#{input_file_name}" }
   let(:subject) { described_class.new(sample_puzzle_input) }
 
-  xdescribe "#part_one" do
+  describe "#part_one" do
     context "with the real puzzle input" do
       let(:subject) { described_class.new(real_puzzle_input) }
 
@@ -13,7 +13,7 @@ RSpec.describe AdventOfCode::DayTen do
         output = subject.part_one
 
         puts "#{indent}part one output: #{output}"
-        expect(output).to eq(0)
+        expect(output).to eq(13720)
       end
     end
 
@@ -22,12 +22,12 @@ RSpec.describe AdventOfCode::DayTen do
         output = subject.part_one
 
         puts "#{indent}part one output: #{output}"
-        expect(output).to eq(0)
+        expect(output).to eq(13140)
       end
     end
   end
 
-  xdescribe "#part_two" do
+  describe "#part_two" do
     context "with the real puzzle input" do
       let(:subject) { described_class.new(real_puzzle_input) }
 
@@ -35,7 +35,13 @@ RSpec.describe AdventOfCode::DayTen do
         output = subject.part_two
 
         puts "#{indent}part two output: #{output}"
-        expect(output).to eq(0)
+        expect(output[0]).to eq("####.###..#..#.###..#..#.####..##..#..#.")
+        expect(output[1]).to eq("#....#..#.#..#.#..#.#..#....#.#..#.#..#.")
+        expect(output[2]).to eq("###..###..#..#.#..#.####...#..#....####.")
+        expect(output[3]).to eq("#....#..#.#..#.###..#..#..#...#....#..#.")
+        expect(output[4]).to eq("#....#..#.#..#.#.#..#..#.#....#..#.#..#.")
+        expect(output[5]).to eq("#....###...##..#..#.#..#.####..##..#..#.")
+
       end
     end
 
@@ -44,7 +50,35 @@ RSpec.describe AdventOfCode::DayTen do
         output = subject.part_two
 
         puts "#{indent}part two output: #{output}"
-        expect(output).to eq(0)
+        expect(output[0]).to eq("##..##..##..##..##..##..##..##..##..##..")
+        expect(output[1]).to eq("###...###...###...###...###...###...###.")
+        expect(output[2]).to eq("####....####....####....####....####....")
+        expect(output[3]).to eq("#####.....#####.....#####.....#####.....")
+        expect(output[4]).to eq("######......######......######......####")
+        expect(output[5]).to eq("#######.......#######.......#######.....")
+      end
+    end
+  end
+
+  context "#check_signal_strength?" do
+    context "when cycle 20, 60, 100, 140, 180, or 220" do
+      it "returns true" do
+        expect(subject.check_signal_strength?(20)).to eq(true)
+        expect(subject.check_signal_strength?(60)).to be true
+        expect(subject.check_signal_strength?(100)).to be true
+        expect(subject.check_signal_strength?(140)).to be true
+        expect(subject.check_signal_strength?(180)).to be true
+        expect(subject.check_signal_strength?(220)).to be true
+      end
+    end
+
+    context "when not cycle 20, 60, 100, 140, 180, or 220" do
+      it "returns false" do
+        222.times do |i|
+          next if [20, 60, 100, 140, 180, 220].include?(i)
+
+          expect(subject.check_signal_strength?(i)).to be false
+        end
       end
     end
   end
